81018d4d99171a60448fc93bae8e6e6ec2c1905c
MoHa270xX <mhmdmedo102030@gmail.com>
2025-12-19 21:50:02 +0200

    feat: Update color scheme and add mockito as a dev dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flowers_app/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.mainColor),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
