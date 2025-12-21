2729db58319155385357f04518a84b01a349d31a
AhmedHussien249 <ahmed.hussien950.ah@gmail.com>
2025-12-21 19:56:37 +0200

    feat: add AppRegex helper for input validation.

--- lib/core/helpers/app_regex.dart
+++ lib/core/helpers/app_regex.dart
@@ -6,11 +6,10 @@ class AppRegex {
   }
 
   static bool isPasswordValid(String password) {
-  return RegExp(
-    r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$'
-  ).hasMatch(password);
-}
-
+    return RegExp(
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$',
+    ).hasMatch(password);
+  }
 
   static bool isPhoneNumberValid(String phoneNumber) {
     return RegExp(r'^(010|011|012|015)[0-9]{8}$').hasMatch(phoneNumber);
