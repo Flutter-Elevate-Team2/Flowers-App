5654eb9560bb3b0745e28bd83f491c92ed837ecb
MALAK0244 <malak.hussein0244@icloud.com>
2025-12-23 02:06:42 +0200

    feat: Integrate `pretty_dio_logger` for enhanced network logging.

--- lib/core/modules/dio_module.dart
+++ lib/core/modules/dio_module.dart
@@ -1,14 +1,31 @@
-import 'dart:developer';
-
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flowers_app/core/auth_interceptors/auth_interceptors.dart';
 import 'package:flowers_app/core/constants/api_constants.dart';
 import 'package:injectable/injectable.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 @module
 abstract class DioModule {
+
   @singleton
-  Dio dio(AuthInterceptor authInterceptor) {
+  PrettyDioLogger get prettyDioLogger {
+    return PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: false,
+      error: true,
+      compact: true,
+      maxWidth: 90,
+    );
+  }
+
+  @singleton
+  Dio dio(
+      AuthInterceptor authInterceptor,
+      PrettyDioLogger dioLogger,
+      ) {
     final dio = Dio(
       BaseOptions(
         baseUrl: ApiConstants.apiBaseUrl,
@@ -23,11 +40,9 @@ abstract class DioModule {
 
     dio.interceptors.add(authInterceptor);
 
-    dio.interceptors.add(LogInterceptor(
-      requestBody: true,
-      responseBody: true,
-      logPrint: (object) => log('DioLog: $object'),
-    ));
+    if (kDebugMode) {
+      dio.interceptors.add(dioLogger);
+    }
 
     return dio;
   }
