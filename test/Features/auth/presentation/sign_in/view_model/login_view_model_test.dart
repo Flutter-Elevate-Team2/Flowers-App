9b6eba5618b994e199e335b523ebe973f668af2e
NaghamArafa <naghamarafa33@gmail.com>
2025-12-24 23:28:10 +0200

    refactor login_view_model_test

--- test/Features/auth/presentation/sign_in/view_model/login_view_model_test.dart
+++ test/Features/auth/presentation/sign_in/view_model/login_view_model_test.dart
@@ -1,8 +1,6 @@
-import 'package:bloc_test/bloc_test.dart';
 import 'package:flowers_app/Features/auth/domain/entities/login_entity.dart';
 import 'package:flowers_app/Features/auth/domain/use_cases/login_usecase.dart';
 import 'package:flowers_app/Features/auth/presentation/sign_in/view_model/login_event.dart';
-import 'package:flowers_app/Features/auth/presentation/sign_in/view_model/login_state.dart';
 import 'package:flowers_app/Features/auth/presentation/sign_in/view_model/login_view_model.dart';
 import 'package:flowers_app/core/base_response/base_response.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -17,7 +15,7 @@ void main() {
 
   setUp(() {
     provideDummy<BaseResponse<LoginEntity>>(
-       ErrorResponse<LoginEntity>(errorMessage: ''),
+      ErrorResponse<LoginEntity>(errorMessage: ''),
     );
     mockLoginUseCase = MockLoginUseCase();
     viewModel = LoginViewModel(mockLoginUseCase);
@@ -35,105 +33,64 @@ void main() {
   );
 
   group('LoginViewModel bloc tests', () {
-    blocTest<LoginViewModel, LoginState>(
-      'emits [loading, success] when login succeeds',
-      build: () {
-        when(
-          mockLoginUseCase.call(
-            email: anyNamed('email'),
-            password: anyNamed('password'),
-          ),
-        ).thenAnswer(
-              (_) async => SuccessResponse<LoginEntity>(data: loginEntity),
-        );
-        return viewModel;
-      },
-      act: (bloc) => viewModel.doIntent(loginEvent),
-      expect: () => [
-        isA<LoginState>().having(
-              (s) => s.loginState?.isLoading,
-          'loading',
-          true,
-        ),
-        isA<LoginState>()
-            .having(
-              (s) => s.loginState?.isLoading,
-          'loading finished',
-          false,
-        )
-            .having(
-              (s) => s.loginState?.data,
-          'success data',
-          loginEntity,
-        )
-            .having(
-              (s) => s.loginState?.errorMessage,
-          'no error',
-          null,
-        ),
-      ],
-      verify: (_) {
-        verify(
-          mockLoginUseCase.call(
-            email: loginEvent.email,
-            password: loginEvent.password,
-          ),
-        ).called(1);
-      },
+    test(
+        'emits [loading, success] when login succeeds',
+            () async {
+          when(
+            mockLoginUseCase.call(
+              email: anyNamed('email'),
+              password: anyNamed('password'),
+            ),
+          ).thenAnswer(
+                (_) async => SuccessResponse<LoginEntity>(data: loginEntity),
+          );
+
+          viewModel.doIntent(loginEvent);
+
+          expect(viewModel.state.loginState?.isLoading, true);
+          await pumpEventQueue();
+
+          expect(viewModel.state.loginState?.isLoading, false);
+          expect(viewModel.state.loginState?.data, loginEntity);
+          expect(viewModel.state.loginState?.errorMessage, null);
+        }
     );
 
-    blocTest<LoginViewModel, LoginState>(
-      'emits [loading, error] when login fails',
-      build: () {
-        when(
-          mockLoginUseCase.call(
-            email: anyNamed('email'),
-            password: anyNamed('password'),
-          ),
-        ).thenAnswer(
-              (_) async =>  ErrorResponse<LoginEntity>(
-            errorMessage: 'Login failed',
-          ),
-        );
-        return viewModel;
-      },
-      act: (cubit) => cubit.doIntent(loginEvent),
-      expect: () => [
-        isA<LoginState>().having(
-              (s) => s.loginState?.isLoading,
-          'loading',
-          true,
-        ),
-        isA<LoginState>()
-            .having(
-              (s) => s.loginState?.isLoading,
-          'loading finished',
-          false,
-        )
-            .having(
-              (s) => s.loginState?.data,
-          'no data',
-          null,
-        )
-            .having(
-              (s) => s.loginState?.errorMessage,
-          'error message',
-          'Login failed',
-        ),
-      ],
-    );
+    test(
+        'emits [loading, error] when login fails',
+            ()async {
+          const testErrorMessage = "Registration failed";
 
-    blocTest<LoginViewModel, LoginState>(
-      'toggles remember me state',
-      build: () => viewModel,
-      act: (cubit) => cubit.doIntent(RememberMeEvent(value: true)),
-      expect: () => [
-        isA<LoginState>().having(
-              (s) => s.isRememberMe,
-          'remember me toggled',
-          true,
-        ),
-      ],
+          when(
+            mockLoginUseCase.call(
+              email: anyNamed('email'),
+              password: anyNamed('password'),
+            ),
+          ).thenAnswer(
+                (_) async =>  ErrorResponse<LoginEntity>(
+              errorMessage: testErrorMessage,
+            ),
+          );
+          viewModel.doIntent(loginEvent);
+          expect(viewModel.state.loginState?.isLoading, true);
+          await pumpEventQueue();
+
+          expect(viewModel.state.loginState?.isLoading, false);
+          expect(viewModel.state.loginState?.data, null);
+          expect(viewModel.state.loginState?.errorMessage, testErrorMessage);
+        }
+    );
+    test(
+      'clicked on remember me button when RememberMeEvent is triggered',
+          () {
+        expect(viewModel.state.isRememberMe, false);
+
+        viewModel.doIntent(
+          RememberMeEvent(value: true),
+        );
+
+        expect(viewModel.state.isRememberMe, true);
+      },
     );
   });
 }
