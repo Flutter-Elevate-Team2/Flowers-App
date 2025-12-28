4847e44f0d3495f4ac530dd34b090def0ef0aa96
MALAK0244 <malak.hussein0244@icloud.com>
2025-12-28 23:29:05 +0200

    feat: Implement home screen with bottom navigation
    This commit introduces the basic structure for the home screen, including a stateful `HomeScreen` widget that manages different views via an `IndexedStack`.
    
    It adds:
    - A `CustomButtonNavigationBar` widget for app navigation (Home, Categories, Cart, Profile).
    - A placeholder `HomeScreenBody` widget.
    - A `BuildContext` extension (`l10n`) for simplified access to localized strings.

---
+++ lib/Features/home/presentation/views/screens/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flowers_app/Features/home/presentation/widgets/custom_button_nav_bar.dart';
+import 'package:flowers_app/Features/home/presentation/widgets/home_screen_body.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreenBody(),
+    Container(),
+    Container(),
+    Container(),
+    // CategoriesScreen(),
+    // CartScreen(),
+    // ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: CustomButtonNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
