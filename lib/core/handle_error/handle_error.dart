b8f99119590783508aa6236ea35a51b29c763118
AhmedHussien249 <ahmed.hussien950.ah@gmail.com>
2025-12-28 10:24:03 +0200

    feat: add handle error from auth branch

--- lib/core/handle_error/handle_error.dart
+++ lib/core/handle_error/handle_error.dart
@@ -27,6 +27,7 @@ class ErrorHandler {
     if (error is Error) {
       debugPrint('🚨 StackTrace: ${error.stackTrace}');
     }
+
     // -------------------------------------------------------------------------
     // SECTION 1: NETWORK & CONNECTION ERRORS
     // -------------------------------------------------------------------------
@@ -105,9 +106,9 @@ class ErrorHandler {
   }
 
   static String _extractErrorMessage(
-      DioException error,
-      String defaultMessage,
-      ) {
+    DioException error,
+    String defaultMessage,
+  ) {
     try {
       final data = error.response?.data;
       if (data is Map<String, dynamic>) {
