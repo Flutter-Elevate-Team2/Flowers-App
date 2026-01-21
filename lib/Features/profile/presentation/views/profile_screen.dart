a951342a42e44868751d24968294075091a6441c
MoHa270xX <mhmdmedo102030@gmail.com>
2026-01-21 18:20:24 +0200

    fix : fix trigger get profile data evet from main to profile screen

--- lib/Features/profile/presentation/views/profile_screen.dart
+++ lib/Features/profile/presentation/views/profile_screen.dart
@@ -1,7 +1,23 @@
+import 'package:flowers_app/Features/profile/presentation/view_model/profile_event.dart';
+import 'package:flowers_app/Features/profile/presentation/view_model/profile_view_model.dart';
 import 'package:flowers_app/Features/profile/presentation/widgets/profile_screen_body.dart';
 import 'package:flutter/material.dart';
-class ProfileScreen extends StatelessWidget {
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Dispatch the GetProfileEvent when the screen is initialized
+    context.read<ProfileViewModel>().doIntent(GetProfileEvent());
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flowers_app/core/controller/session_controller.dart';
 import 'package:flowers_app/core/l10n/app_localizations.dart';
 import 'package:flowers_app/core/theming/app_theming.dart';
 import 'package:flowers_app/core/helpers/session_expired_handler.dart';
-import 'package:flowers_app/Features/profile/presentation/view_model/profile_event.dart';
+
 import 'package:flowers_app/Features/profile/presentation/view_model/profile_view_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -49,12 +49,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) =>
-              getIt<ProfileViewModel>()..doIntent(GetProfileEvent()),
-        ),
-      ],
+      providers: [BlocProvider(create: (context) => getIt<ProfileViewModel>())],
       child: MaterialApp.router(
         routerConfig: AppRouter.router,
         debugShowCheckedModeBanner: false,
