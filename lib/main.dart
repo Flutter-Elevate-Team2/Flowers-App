e4de9d1d0c045039a9c5fcba1ae0fd8a5b11a98a
NaghamArafa <naghamarafa33@gmail.com>
2026-02-05 01:29:12 +0200

    feat: Integrate Firebase Crashlytics for error reporting and analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flowers_app/Features/profile/presentation/view_model/profile_view_model.dart';
 import 'package:flowers_app/Features/user_address/presentation/view_model/user_address_event.dart';
 import 'package:flowers_app/Features/user_address/presentation/view_model/user_address_view_model.dart';
@@ -11,6 +12,7 @@ import 'package:flowers_app/core/l10n/app_localizations.dart';
 import 'package:flowers_app/core/l10n/view_model/language_cubit.dart';
 import 'package:flowers_app/core/theming/app_theming.dart';
 import 'package:flowers_app/core/widget/selected_address_cubit.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -20,7 +22,7 @@ import 'Features/order/presentation/cart/view_model/cart_view_model.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -28,6 +30,11 @@ void main() async {
   await dotenv.load(fileName: ".env");
   await configureDependencies();
 
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const MyApp());
 }
 
