0d0610fe9c154a207d60d6425af1c523739b4afb
NaghamArafa <naghamarafa33@gmail.com>
2026-02-06 17:50:52 +0200

    feat: Add Firebase BoM and dependencies for Crashlytics and Analytics

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,16 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.firebase.crashlytics") version "3.0.6" apply false
 
+}
+dependencies {
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
+
+    // Add the dependencies for the Crashlytics and Analytics libraries
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-crashlytics")
+    implementation("com.google.firebase:firebase-analytics")
 }
 
 android {
